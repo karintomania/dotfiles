52ee5073faba2358450f1845e9befcfabf335b44
karintomania <19652340+karintomania@users.noreply.github.com>
2024-12-19 11:53:43 +0000

    add open from clipboard short cut

--- .config/helix/open-from-clipboard.sh
+++ .config/helix/open-from-clipboard.sh
@@ -1,7 +1,8 @@
 paths=$(xclip -o -selection c)
 
 if [[ -e "$paths" ]]; then
-    zellij action toggle-floating-panes
+    # zellij action toggle-floating-panes
+    zellij action move-focus left
     zellij action write 27 # send <Escape> key
     zellij action write-chars ":open $paths"
     zellij action write 13 # send <Enter> key
